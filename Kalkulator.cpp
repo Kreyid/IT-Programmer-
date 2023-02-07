715c32e6b18f5740d3edc7e7e8f3aea7ca70d0aa
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-07 12:55:29 +0000

    Kalkulator
    Etot kod kak kalkulator vivodit na ekran vvedennie dva chisla s otvetom

---
+++ Kalkulator.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.4.33213.308
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Bilolexam.cpp", "Bilolexam.cpp\Bilolexam.cpp.vcxproj", "{529FFC08-62AD-4699-AE43-63A133412AC6}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Debug|x64.ActiveCfg = Debug|x64
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Debug|x64.Build.0 = Debug|x64
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Debug|x86.ActiveCfg = Debug|Win32
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Debug|x86.Build.0 = Debug|Win32
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Release|x64.ActiveCfg = Release|x64
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Release|x64.Build.0 = Release|x64
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Release|x86.ActiveCfg = Release|Win32
+		{529FFC08-62AD-4699-AE43-63A133412AC6}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {C672ED2B-C313-4A63-8716-BD779D0B6A45}
+	EndGlobalSection
+EndGlobal
