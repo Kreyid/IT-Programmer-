4a2ee5c924f8af80c6842ae648f4380388b92e12
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-07 22:02:25 +0500

    Parol
    Imenno takimie kodi ispolzuyut v telefonah ili eshe tam gde est paroli

---
+++ parol.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ int main()
+ {
+     int num;
+     do {
+     cout <<"vvedite cislo (100 - dlya vixoda):";
+     cin >> num;
+}while(num != 100);
+ return 0;
+}
