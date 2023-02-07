3ed6ef544ba11378b9f610d7fd8766859e769e8d
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-07 22:12:12 +0500

    Magicheskoe chislo
    Etot kod randomno vibiraet chislo i vi doljni ugadat eto chislo

---
+++ Magicheskoe chislo.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	int magic;
+	int guess;
+	magic = rand();
+	do {
+		cout << "Vedite svoy variant magicheskogo chisla: ";
+		cin >> guess;
+		if (guess = magic) { cout << "** Pravilno **"; }
+		else {
+			cout << "Ochen jal, no eto nepravilno. ";
+			if (guess > magic)
+				cout << "Vash variant bolshe magicheskogo chisla.\n";
+			else cout << "Vash variant menshe magicheskogo chisla.\n";
+		}
+	} while (guess != magic);
+	return 0;
+}
