c21564373379f141c6629a46bf6a8f1b7a3a785b
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-09 18:07:08 +0500

    Kalkulaer upgrade
    Этот калькулятор выводит на экран все решения двух чисел

---
+++ Kalkulator with answer.cpp.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double x, y;
+    int i;
+    x = 0;
+    y = 0;
+    char mul;
+    while (true)
+    {
+        cout << "V pervoy strochke viydet slojenie, na vtoroy umnojenie, na tretey vichetanie, na posledney delenie \n";
+        cout << "\nVvedite lubie chisla: ";
+
+        cin >> x >> y;
+        cout << "\n";
+
+        system("color 09");
+
+        for (i = 0; i < x, y; i++) {
+            cout << x + y;
+            cout << "\n";
+
+            cout << x * y;
+            cout << "\n";
+
+            for (i = 0; i < x, y; i--) {
+                cout << x - y;
+                cout << "\n";
+
+                cout << x / y;
+
+
+                return 0;
+            }
+        }
+    }
+}
