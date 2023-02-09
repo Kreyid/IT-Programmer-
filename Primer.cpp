ea84716eef6bd5958fa43bef91ea4c11f9eb277c
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-09 17:37:48 +0500

    Add files via upload
    Etot kod vidaet vam randomnie primeri, vi doljni ih reshit

---
+++ Primer.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+void drill();
+int count;
+int num_right;
+int main()
+{
+    cout << "Nechta mashq beray?: ";
+    cin >> count;
+    num_right = 0;
+    do {
+        drill(); count--;
+    } while (count);
+    cout << "Siz  " << num_right << "ta Mashqlarga javob berdingiz\n";
+    return 0;
+}
+void drill()
+{
+    int count;
+    int a, b, ans;
+    a = rand() % 100;
+    b = rand() % 100;
+    for (count = 0; count < 2; count++) {
+        cout << "Shu sonlarni qo'shsak nechi bo'ladi?" << a << " + " << b << " ? ";
+        cin >> ans;
+        if (ans == a + b) {
+            cout << "Malades san matematiksan\n";
+            num_right++;
+            return;
+        }
+    }
+    cout << "Uzur oshna urunishlarin qolmadi.\n";
+    cout << "Javobi " << a + b << '\n';
+}
