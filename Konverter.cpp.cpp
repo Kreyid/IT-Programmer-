4a0ebf3f43743fdb50d1a3259f5e99b5364e4fd6
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-09 18:26:21 +0500

    Konverter som i dollar
    Этот код настоящий конвертер, переведёт доллары в сум

---
+++ Konverter.cpp.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int dollar, som;
+	cout << "Vvedite skolko dollarov hotite perevesti: ";
+	cin >> dollar;
+	som = dollar * 11300;
+	cout << "Somov: " << som;
+	return 0;
+}
