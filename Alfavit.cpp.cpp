5d797540047b2a97b2779f196376953c8ac77283
Kreyid <124684793+Kreyid@users.noreply.github.com>
2023-02-09 18:41:26 +0500

    Alfavit
    Этот код выводит на экран алфавит

---
+++ Alfavit.cpp.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter;
+    for (letter = 'A'; letter <= 'Z'; letter++) cout << letter;
+    system("color 0A");
+    return 0;
+}
